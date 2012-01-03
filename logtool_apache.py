768ad437955bd3d2d8499967e778b2208c750a29
ambivalent <amb@grid.(none)>
2012-01-03 21:40:41 +0100

    fixed some issues

--- logtool_apache.py
+++ logtool_apache.py
@@ -2,24 +2,39 @@
 import  re
 import sys
 import urllib2
+from compiler.syntax import check
 
-
-
+def tor_exit_nodes():
+    list_tor_exit_nodes = "http://torstatus.blutmagie.de/ip_list_exit.php/Tor_ip_list_EXIT.csv"
+    list_tor_exit_nodes = urllib2.urlopen(list_tor_exit_nodes).read()
+    list_tor_exit_nodes = list_tor_exit_nodes.split("\n")
+    #print list
+    return list_tor_exit_nodes
+    
 def is_tor_exit(ip):
     list = "http://torstatus.blutmagie.de/ip_list_exit.php/Tor_ip_list_EXIT.csv"
     list = urllib2.urlopen(list).read()
     list = list.split("\n")
-    print list
-    ip_tor_exit=False
+    #print list
+    ip_tor_exit = False
     for i in range(len(list)):
-        if list[i]==ip:
+        if list[i] == ip:
             return True
-        
-        
+                
+def check_tor_nodes(logRows):
+    #check for tor exit nodes present in the log used for requesting data 
+    list_tor=[]
+    list_tor = tor_exit_nodes()
+    for tor_ip in range(len(list_tor)):
+        for row in range(len(logRows)):
+            if str(logRows[row][0]) == str(list_tor[tor_ip]):
+                 print str(logRows[row][0]) + "\t" + str(logRows[row][3]) + "\t" + str(logRows[row][4]) + "\t" + str(logRows[row][5]) + "\t" + str(logRows[row][7]) + "\t" + str(logRows[row][8])
+                 #todo:save in file
+            
 
 def readLogLine(pathToLog):
     f = open(pathToLog, 'r')
-    row=[]
+    row = []
     for line in f:
         #print line
         row.append(re.findall('\[[^\]]*\]|\"[^\"]*\"|\S+', line))
@@ -37,17 +52,17 @@ def printLines(logRows):
     #7 - referer
     #8 - agent
     for row in range(len(logRows)):
-        print str(logRows[row][0])+"\t"+str(logRows[row][3])+"\t"+str(logRows[row][4])+"\t"+str(logRows[row][5])+"\t"+str(logRows[row][7])+"\t"+str(logRows[row][8])
+        print str(logRows[row][0]) + "\t" + str(logRows[row][3]) + "\t" + str(logRows[row][4]) + "\t" + str(logRows[row][5]) + "\t" + str(logRows[row][7]) + "\t" + str(logRows[row][8])
     
 
 if __name__ == '__main__':
      if len(sys.argv) != 2:
           print('Problem with Apache access log path\n')
-          print('Debug info:'+str(sys.argv)+'\n')
+          print('Debug info:' + str(sys.argv) + '\n')
      elif len(sys.argv) == 2:
-          logRows=readLogLine(sys.argv[1])
-          print 'Lines:'+str(len(logRows))
-          print is_tor_exit('212.13.195.157')
+          logRows = readLogLine(sys.argv[1])
+          print 'Lines:' + str(len(logRows))
+          check_tor_nodes(logRows)
           #printLines(logRows)
 
         
