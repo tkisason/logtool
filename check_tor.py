e586bc03377c9827e501469ac750ecb4c377c5a6
Tonimir Kisasondi <kisasondi@gmail.com>
2012-01-04 12:49:43 +0100

    Added option to find any TOR server

--- check_tor.py
+++ check_tor.py
@@ -6,4 +6,18 @@ def is_tor_exit(ip):
     list = "http://torstatus.blutmagie.de/ip_list_exit.php/Tor_ip_list_EXIT.csv"
     list = urllib2.urlopen(list).read()
     list = list.split("\n")
-    print list
+    if ip in list:
+        return 1
+    else:
+        return 0
+
+def is_tor_server(ip):
+    list = "http://torstatus.blutmagie.de/ip_list_all.php/Tor_ip_list_ALL.csv"
+    list = urllib2.urlopen(list).read()
+    list = list.split("\n")
+    if ip in list:
+        return 1
+    else:
+        return 0
+
+
