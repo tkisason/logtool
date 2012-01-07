1f54c92af918e14b151c63b2bd433ad36620e68f
ambivalent <amb@tr0n.(none)>
2012-01-08 00:22:00 +0100

    ua parsing & writing in a file

--- logtool_apache.py
+++ logtool_apache.py
@@ -4,6 +4,7 @@ import csv
 import sys
 import urllib2
 from compiler.syntax import check
+from uasparser import UASparser  
 
 def tor_exit_nodes():
     tor_exit_nodes = "http://torstatus.blutmagie.de/ip_list_exit.php/Tor_ip_list_EXIT.csv"
@@ -32,15 +33,6 @@ def check_tor_nodes(logRows):
                  print str(logRows[row][0]) + "\t" + str(logRows[row][3]) + "\t" + str(logRows[row][4]) + "\t" + str(logRows[row][5]) + "\t" + str(logRows[row][7]) + "\t" + str(logRows[row][8])
                  #todo:save in file
 
-def use_rest_ua_detect(strUA):
-    url_api = "http://www.useragentstring.com/?uas=" + strUA + "&getText=all"
-    print "debuh url_api: " + url_api
-    list = urllib2.urlopen(url_api).read()
-    print "debug list: " + list
-    list = list.split(";")
-    
-               
-
 def readLogLine(pathToLog):
     f = open(pathToLog, 'r')
     row = []
@@ -62,70 +54,64 @@ def printLines(logRows):
     #8 - agent
     for row in range(len(logRows)):
         print str(logRows[row][0]) + "\t" + str(logRows[row][3]) + "\t" + str(logRows[row][4]) + "\t" + str(logRows[row][5]) + "\t" + str(logRows[row][7]) + "\t" + str(logRows[row][8])
+        
+def uas_parser(logRows=[]):
+    uas_parser = UASparser()  
+    total_rows_in_log=len(logRows)
+    #only 'ua_icon' or 'os_icon' or both are allowed in entire_url
     
-def UA_match(logRows, UA):
-    #checking user agents
-    found_UA = False
-    not_recognized_UA = []
-    for row_log in range(len(logRows)):
-        for row_csv in range(len(UA)):
-            UA_log = str(logRows[row_log][8].replace('"', ''))    
-            UA_csv = str(UA[row_csv][1])
-            #print "cur_row_log:    " + cur_row_log + "    csv r:    " + csv_row + "\n"
-            if UA_log == UA_csv:
-                found_UA = True
-                print UA[row_csv][0] + "\t" + UA[row_csv][6]
-            else:
-                not_recognized_UA.append(row_log)
-                    
-   
-        #print 'Defected or unknown user agent'
-        #add to some list for further analysis
-            
-    
-def UA_list():
-    #usage
-    #ua=list_User_Agents()
-    #ua[123][0]+ua[123][1]+ua[123][3]
-    
-    uaCvs = 'browscap.csv'
-    uaList = csv.reader(open(uaCvs, 'rb'), delimiter=',')
-    UA = []
-    for row in uaList:
-        UA.append(row)
-    return UA
-    
+    out = open('ua.log', 'w')
+    for row_log in range(total_rows_in_log):
+        #todo : progress bar
+        #perc=(float(row_log)/float(total_rows_in_log))*100
+        #progress(perc)
+        sys.stdout.write("\rParsing row log #%s of %s" % (str(row_log),str(total_rows_in_log)))
+        sys.stdout.flush()   
+        ua_string = str(logRows[row_log][8])
+        parsed_ua = uas_parser.parse(ua_string)
+        if(parsed_ua['ua_name'] == "unknown"):
+            out.write("%s\t%s\t%s\t%s\n" % (parsed_ua['typ'], parsed_ua['ua_name'], parsed_ua['os_name'], ua_string))
+        else:
+            out.write("%s\t%s\t%s\n" % (parsed_ua['typ'], parsed_ua['ua_name'], parsed_ua['os_name']))
+        #save in file
+    out.close()
+       
 usage = """
         ----------------------------------------------------------------------
         logtool apache, find interesting patterns in apache access log files
             
-        usage: logtool.py [infile] 
+        usage: logtool_apache.py [infile] 
         options:
         1. check for tor nodes in log file
-        2. export user agents 
+        2. export user agents
+        3. ...
+        4. ...
+        5. exit app
         ----------------------------------------------------------------------
       """
 if __name__ == '__main__':
-   
+    option = -1
     if len(sys.argv) != 2:
          print('Problem with Apache access log path\n' + 'Debug info:' + str(sys.argv) + '\n')
     elif len(sys.argv) == 2:
          logRows = readLogLine(sys.argv[1])
          print 'Log:' + sys.argv[1]
          print 'No of records in log:' + str(len(logRows))
-         option = raw_input('Enter you choice (for help enter 0):')
-         if int(option) == 0:
-              print usage
-              option = raw_input('Enter you choice (for help enter 0):')
-         elif int(option) == 1:
-              print check_tor_nodes(logRows)
-              option = raw_input('Enter you choice (for help enter 0):')
-         elif int(option) == 2:
-              UAList = UA_list()
-              UA_match(logRows, UAList)
-              option = raw_input('Enter you choice (for help enter 0):')
-         else:
-              print 'Problem with option see usage instructions!\n'   
+         while(int(option) == -1):
+             option = raw_input('Enter you choice (for help enter 0):')
+             if int(option) == 0:
+                  print usage
+                  option = -1
+             elif int(option) == 1:
+                  print check_tor_nodes(logRows)
+                  option = -1
+             elif int(option) == 2:
+                  ua_list = uas_parser(logRows)
+                  option = -1
+             elif int(option) == 5:
+                break     
+             else:
+                  print 'Problem with option see usage instructions!\n'   
         
           #printLines(logRows)
           #UAlist()
