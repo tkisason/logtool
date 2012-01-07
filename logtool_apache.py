1c5f64bc674d8167a608107e5fe7427915ffe93b
ambivalent <amb@tr0n.(none)>
2012-01-07 19:17:42 +0100

    some testing of user agent stuff

--- logtool_apache.py
+++ logtool_apache.py
@@ -24,14 +24,22 @@ def is_tor_exit(ip):
                 
 def check_tor_nodes(logRows):
     #check for tor exit nodes present in the log used for requesting data 
-    list_tor=[]
+    list_tor = []
     list_tor = tor_exit_nodes()
     for tor_ip in range(len(list_tor)):
         for row in range(len(logRows)):
             if str(logRows[row][0]) == str(list_tor[tor_ip]):
                  print str(logRows[row][0]) + "\t" + str(logRows[row][3]) + "\t" + str(logRows[row][4]) + "\t" + str(logRows[row][5]) + "\t" + str(logRows[row][7]) + "\t" + str(logRows[row][8])
                  #todo:save in file
-            
+
+def use_rest_ua_detect(strUA):
+    url_api="http://www.useragentstring.com/?uas="+strUA+"&getText=all"
+    print "debuh url_api: "+url_api
+    list=urllib2.urlopen(url_api).read()
+    print "debug list: "+list
+    list=list.split(";")
+    
+               
 
 def readLogLine(pathToLog):
     f = open(pathToLog, 'r')
@@ -55,35 +63,70 @@ def printLines(logRows):
     for row in range(len(logRows)):
         print str(logRows[row][0]) + "\t" + str(logRows[row][3]) + "\t" + str(logRows[row][4]) + "\t" + str(logRows[row][5]) + "\t" + str(logRows[row][7]) + "\t" + str(logRows[row][8])
     
-def printUserAgents(logRows):
-    whitelist=['Mozilla/4.0','Opera','MSIE']
-    for row in range(len(logRows)):
-        print str(logRows[row][8]+"\n")
-
-def list_User_Agents():
+def UA_match(logRows, UA):
+    #checking user agents
+    found_UA = False
+    not_recognized_UA = []
+    for row_log in range(len(logRows)):
+        for row_csv in range(len(UA)):
+            UA_log = str(logRows[row_log][8].replace('"', ''))    
+            UA_csv = str(UA[row_csv][1])
+            #print "cur_row_log:    " + cur_row_log + "    csv r:    " + csv_row + "\n"
+            if UA_log == UA_csv:
+                found_UA = True
+                print UA[row_csv][0] + "\t" + UA[row_csv][6]
+            else:
+                not_recognized_UA.append(row_log)
+                    
+   
+        #print 'Defected or unknown user agent'
+        #add to some list for further analysis
+            
+    
+def UA_list():
     #usage
     #ua=list_User_Agents()
     #ua[123][0]+ua[123][1]+ua[123][3]
-
     
-    uaCvs='browscap.csv'
-    uaList=csv.reader(open(uaCvs,'rb'),delimiter=',',quotechar='|')
-    UA=[]
+    uaCvs = 'browscap.csv'
+    uaList = csv.reader(open(uaCvs, 'rb'), delimiter=',')
+    UA = []
     for row in uaList:
         UA.append(row)
-        
     return UA
     
-
+usage = """
+        ----------------------------------------------------------------------
+        logtool apache, find interesting patterns in apache access log files
+            
+        usage: logtool.py [infile] 
+        options:
+        1. check for tor nodes in log file
+        2. export user agents 
+        -----------------------------------------------------------------------
+      """
 if __name__ == '__main__':
-     if len(sys.argv) != 2:
-          print('Problem with Apache access log path\n')
-          print('Debug info:' + str(sys.argv) + '\n')
-     elif len(sys.argv) == 2:
-          logRows = readLogLine(sys.argv[1])
-          print 'Lines:' + str(len(logRows))
-          #check_tor_nodes(logRows)
+   
+    if len(sys.argv) != 2:
+         print('Problem with Apache access log path\n' + 'Debug info:' + str(sys.argv) + '\n')
+    elif len(sys.argv) == 2:
+         logRows = readLogLine(sys.argv[1])
+         print 'Log:' + sys.argv[1]
+         print 'No of records in log:' + str(len(logRows))
+         option = raw_input('Enter you choice (for help enter 0):')
+         if int(option) == 0:
+              print usage
+              option = raw_input('Enter you choice (for help enter 0):')
+         elif int(option) == 1:
+              print check_tor_nodes(logRows)
+              option = raw_input('Enter you choice (for help enter 0):')
+         elif int(option) == 2:
+              UAList = UA_list()
+              UA_match(logRows, UAList)
+              option = raw_input('Enter you choice (for help enter 0):')
+         else:
+              print 'Problem with option see usage instructions!\n'   
+        
           #printLines(logRows)
-          #printUserAgents(logRows)
-          UAlist()
+          #UAlist()
         
